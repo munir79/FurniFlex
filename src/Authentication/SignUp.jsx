3bcc0da07d4f1efd2f05d6f4a1969336dcd4749d
munir79 <jakirhossainmunir79@gmail.com>
2024-09-05 22:46:55 +0600

    signUp with firebase

--- src/Authentication/SignUp.jsx
+++ src/Authentication/SignUp.jsx
@@ -1,67 +1,101 @@
 
 import { Link } from 'react-router-dom';
-import logo1 from '../assets/Logo/FurniFlex.png'
+
+import { FcGoogle } from "react-icons/fc";
+import { GrApple } from "react-icons/gr";
+import { useContext } from 'react';
+import { AuthContext } from '../Context/AuthProvider';
+import Swal from 'sweetalert2';
 const SignUp = () => {
-    return (
-       <div className=' bg-[ #F5F5F5] w-[600px] h-[619px] border-solid rounded-sm border-1 mx-auto'>
-         <div>
-           <p className='text-2xl font-semibold text-center'>Welcome To</p>
-           <h3 className='text-4xl mt-3 text-center font-bold'>Furni<span className='text-[#1E99F5]'>Flex</span></h3>
-           <p className='font-medium mt-2 text-center text-xl text-[#707070]'>Signup for purchase your desire products</p>
-           
- <form >
+const {CreateUser}=useContext(AuthContext);
+  const handleRegistration=event=>{
+    event.preventDefault();
+    const form=event.target;
+    const firstName=form.firstname.value;
+    const lastName=form.lastname.value;
+    const email=form.email.value;
+    const password=form.password.value;
 
- <div className='flex mt-5 items-center justify-center flex-col md:flex-row gap-2'>
+    CreateUser(email,password)
+    .then(result=>{
+      const user=result.user;
+      console.log(user);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "SignUp Sucessfully",
+        showConfirmButton: false,
+        timer: 1500
+      });
+      form.reset(" ");
+    })
+    .catch(error=>{
+      console.log(error);
+    })
 
+    console.log(firstName,lastName,email,password);
+  }
+  return (
+    <div className=' bg-[#FAFAFA] w-[600px] h-[719px] border-solid border-1 rounded-lg mx-auto'>
+      <div className='mt-10 p-10'>
+        <p className='text-2xl  font-semibold text-center'>Welcome To</p>
+        <h3 className='text-4xl mt-3 text-center font-bold'>Furni<span className='text-[#1E99F5]'>Flex</span></h3>
+        <p className='font-medium mt-2 text-center text-xl text-[#707070]'>Signup for purchase your desire products</p>
 
-<label className="input input-bordered flex flex-col ">
-FirstName(optional)<input type="text" className="grow  w-[259px] h-[52px]" placeholder="Jordan" />
-</label>
-<label className="input  input-bordered flex flex-col  ">
-LastName(optional)<input type="text" className="grow w-[259px] h-[52px]" placeholder="Ken" />
-</label>
-</div>
+        <form onSubmit={handleRegistration} >
 
-
-<label className="input mt-5 input-bordered flex flex-col  ">
-  Email
-  <input type="text" className="grow" placeholder="jordan@email.com" />
-</label>
-<label className="input mt-5 input-bordered flex flex-col  ">
-Password
-  <input type="password" className="grow" placeholder=" " />
-</label>
-
-
-<div className=" mt-5">
-  <label className="cursor-pointer  ">
-    
-    <input type="checkbox" defaultChecked className="checkbox  checkbox-info" />
-    <span className="  text-[14px] font-medium text-[#000000]">I agree to the Terms & Policy</span>
-  </label>
-</div>
-<button type='submit'  className="btn mt-5 w-full btn-active btn-neutral">SignUp</button>
- </form>
-
- <div className="flex items-center justify-center my-4">
-  <div className="border-t border-gray-300 flex-grow"></div>
-  <span className="mx-4 text-gray-500">or</span>
-  <div className="border-t border-gray-300 flex-grow"></div>
-</div>
-           
+          <div className='flex  flex-col md:flex-row mt-5    gap-2'>
 
 
+            <label className="input m-2 input-bordered flex flex-col ">
+              FirstName(optional)<input type="text" name='firstname' className=" w-full md:w-[209px] h-[52px]" placeholder="Jordan" />
+            </label>
+            <label className="input  m-2 input-bordered flex flex-col  ">
+              LastName(optional)<input type="text" name='lastname' className=" w-full md:w-[209px] h-[52px]" placeholder="Ken" />
+            </label>
+          </div>
 
 
+          <label className="input m-3 mt-5 input-bordered flex flex-col  ">
+            Email
+            <input type="text" name='email' className="grow" placeholder="jordan@email.com" />
+          </label>
+          <label className="input m-3 mt-5 input-bordered flex flex-col  ">
+            Password
+            <input type="password" name='password' className="grow" placeholder=" " />
+          </label>
 
+
+          <div className=" m-4 gap-1 mt-5">
+            <label className="cursor-pointer space-x-2  ">
+
+              <input type="checkbox" defaultChecked className="checkbox mt-1 checkbox-info" />
+              <span className="  text-[14px] mt-0 font-medium text-[#000000]">I agree to the <span className='underline'>Terms & Policy</span></span>
+            </label>
+          </div>
+          <button type='submit' className="btn m-3  mt-2 w-[522px] btn-active btn-neutral">SignUp</button>
+        </form>
+
+        <div className="flex items-center justify-center my-4">
+          <div className="border-t border-gray-300 flex-grow"></div>
+          <span className="mx-4 text-gray-500">or</span>
+          <div className="border-t border-gray-300 flex-grow"></div>
         </div>
 
-        <div className='flex'>
-        <button type='submit'  className="btn mt-5 w-1/2 gap-2 btn-active btn-neutral">SignUp</button><button type='submit'  className="btn mt-5 w-1/2 btn-active btn-neutral">SignUp</button>
-        </div>
-        <p className='text-center font-medium'>Have an account ?<a href=""><Link className='text-blue-500' to='/login'>sign in</Link> </a> </p>
-       </div>
-    );
+
+
+
+
+
+      </div>
+
+      <div className='flex flex-col md:flex-row md:justify-between'>
+        <button type='submit' className="btn m-3 mt-5 w-full md:w-[278px] h-[52px] gap-2 btn-active btn-ghost"> <FcGoogle className='w-[20px] h-[23px]' />SignUp with Google</button>
+        <button type='submit' className="btn m-3  mt-5 w-full md:w-[278px] h-[52px] btn-active btn-ghost"> <GrApple className='w-[20px] h-[23px]' />SignUp with Apple</button>
+      </div>
+      <p className='text-center font-medium'>Have an account ?<a href=""><Link className='text-blue-500' to='/login'>sign in</Link> </a> </p>
+    </div>
+  );
 };
 
 export default SignUp;
--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,11 +1,15 @@
 import { createContext } from "react";
 import app from "../FireBase/Firebase.init";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 export const AuthContext=createContext(null);
 const auth=getAuth(app);
 const AuthProvider = ({children}) => {
+    
+   const CreateUser=(email,password)=>{
+    return createUserWithEmailAndPassword(auth,email,password);
+   }
 
-    const AuthInfo={}
+    const AuthInfo={CreateUser}
     return (
         <AuthContext.Provider value={AuthInfo}>
             {children}
